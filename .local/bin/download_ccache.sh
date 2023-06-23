a735ebdd5dbfe53a4164db32d18ff0a7f9a842ed
jens <jens@doellmann.com>
2023-06-23 11:32:29 +0200

    use `mktemp` with cleanup

--- .local/bin/download_ccache.sh
+++ .local/bin/download_ccache.sh
@@ -6,6 +6,12 @@ declare -r reset="\033[0m"
 declare -r red="\033[0;31m"
 declare -r green="\033[0;32m"
 
+declare -r version=${1#v}
+
+# https://manpages.debian.org/mktemp
+output_dir=$(mktemp --directory)
+declare -r output_dir
+
 __usage()
 {
   echo "Usage: $(basename "${BASH_SOURCE[0]}") <version>"
@@ -21,7 +27,15 @@ __error() {
   exit 1
 }
 
-declare -r version=${1#v}
+__cleanup ()
+{
+  echo "Cleaning up temporary directory: $output_dir"
+  # https://manpages.debian.org/rm
+  rm --recursive "$output_dir"
+}
+
+# https://www.gnu.org/software/bash/manual/html_node/Bourne-Shell-Builtins.html#index-trap
+trap __cleanup EXIT
 
 if [[ -z $version ]]
 then
@@ -36,11 +50,11 @@ fi
 
 declare -r binary="ccache-$version-linux-x86_64.tar.xz"
 declare -r binary_uri="https://github.com/ccache/ccache/releases/download/v$version/$binary"
-declare -r binary_output="$HOME/tmp/$binary"
+declare -r binary_output="$output_dir/$binary"
 
 declare -r signature="ccache-$version-linux-x86_64.tar.xz.asc"
 declare -r signature_uri="https://github.com/ccache/ccache/releases/download/v$version/$signature"
-declare -r signature_output="$HOME/tmp/$signature"
+declare -r signature_output="$output_dir/$signature"
 
 # https://manpages.debian.org/curl
 curl --location \
@@ -53,7 +67,7 @@ then
 fi
 
 # https://manpages.debian.org/tar
-tar --extract --xz --file "$binary_output" --directory "$HOME/tmp"
+tar --extract --xz --file "$binary_output" --directory "$output_dir"
 
 declare -r executable="${binary_output%.tar.xz}/ccache"
 declare -r executable_output="$HOME/.local/bin"
