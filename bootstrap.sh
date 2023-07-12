b3d20c45ad3ef21859d432ea6bac124af83f2f6d
jens <jens@doellmann.com>
2023-07-12 19:57:58 +0200

    add some logging

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,17 +18,34 @@ __bootstrap() {
   # https://neovim.io/doc/user/builtin.html#stdpath()
 
   if type -f nvim > /dev/null 2>&1; then
-    local -r nvim_config_home=$(nvim --noplugin --headless --cmd 'call writefile([stdpath("config")], "/dev/stdout", "b") | q')
+    # https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#index-mapfile
+    mapfile -t < <(nvim --noplugin --headless --cmd 'call writefile([stdpath("config"), stdpath("data")], "/dev/stdout", "b") | q')
+
+    local -r nvim_config_home="${MAPFILE[0]}"
     local -r default_nvim_config_home="$HOME/.config/nvim"
 
-    local -r nvim_data_home=$(nvim --noplugin --headless --cmd 'call writefile([stdpath("data")], "/dev/stdout", "b") | q')
+    local -r nvim_data_home="${MAPFILE[1]}"
     local -r default_nvim_data_home="$HOME/.local/share/nvim"
 
     if [[ $default_nvim_config_home != "$nvim_config_home" ]]; then
+      cat <<- EOF
+
+[Neovim] Detected a non-default ($default_nvim_config_home) config directory, also copying files to: $nvim_config_home
+[Neovim] See https://neovim.io/doc/user/starting.html#standard-path
+
+EOF
+
       rsync --no-perms --archive --verbose --human-readable --exclude-from="$root/.rsyncignore" "$default_nvim_config_home/" "$nvim_config_home"
     fi
 
     if [[ $default_nvim_data_home != "$nvim_data_home" ]]; then
+    cat <<- EOF
+
+[Neovim] Detected a non-default ($default_nvim_data_home) data directory, also copying files to: $nvim_data_home
+[Neovim] See https://neovim.io/doc/user/starting.html#standard-path
+
+EOF
+
       rsync --no-perms --archive --verbose --human-readable --exclude-from="$root/.rsyncignore" "$default_nvim_data_home/" "$nvim_data_home"
     fi
   fi
