a581a504772f5d9c2d2fb07841dc08ddc8177152
jens <jens@doellmann.com>
2023-07-30 20:21:25 +0200

    more completion

--- unix/.local/share/bash-completion/completions/install-ccache.bash
+++ unix/.local/share/bash-completion/completions/install-ccache.bash
@@ -13,10 +13,18 @@ __start_install_ccache() {
   __install_ccache_debug '========= starting install-ccache completion logic =========='
   __install_ccache_debug "cur = $cur, prev = $prev, words[*] = ${words[*]}, #words[@] = ${#words[@]}, cword is $cword"
 
+  case "$prev" in
+  --prefix)
+    # Complete directories after prefix option
+    _filedir -d
+    ;;
+  esac
+
   if [[ $cur = -* ]]; then
+    # Needs not to be expanded
     # shellcheck disable=SC2016
     mapfile -t COMPREPLY < <(compgen -W '$(_parse_help "$1")' -- "$cur")
   fi
 }
 
-complete -o default -F __start_install_ccache install-ccache
+complete -F __start_install_ccache install-ccache
