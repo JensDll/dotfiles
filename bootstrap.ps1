57fa76b1f15c07b0ca5607c0aa20cc6c067708dd
jens <jens@doellmann.com>
2023-08-11 01:49:42 +0200

    add oh my posh

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -14,11 +14,14 @@ param(
   [Parameter(ParameterSetName = 'Dotfiles')]
   [switch]$Yes,
   [Parameter(ParameterSetName = 'Registry')]
-  [switch]$Registry
+  [switch]$Registry,
+  [Parameter(ParameterSetName = 'Environment')]
+  [switch]$Environment
 )
 
 function Install-Dotfiles() {
   Copy-Item -Path "$PSScriptRoot\windows\profile.ps1" -Destination $PROFILE -Verbose
+  Copy-Item -Path "$PSScriptRoot\windows\.config" -Destination $HOME -Verbose -Recurse -Force
   Get-ChildItem -Path "$PSScriptRoot\unix" -File | Select-Object -ExpandProperty FullName | Copy-Item -Destination $HOME -Verbose
 }
 
@@ -26,8 +29,21 @@ function Install-Registry() {
   regedit.exe /s "$PSScriptRoot\windows\setup.reg"
 }
 
+function Install-Environment() {
+  # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+  [System.Environment]::SetEnvironmentVariable('XDG_DATA_HOME', "$HOME\.local\share", [System.EnvironmentVariableTarget]::User)
+  [System.Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME', "$HOME\.config", [System.EnvironmentVariableTarget]::User)
+  [System.Environment]::SetEnvironmentVariable('XDG_STATE_HOME', "$HOME\.local\state", [System.EnvironmentVariableTarget]::User)
+  [System.Environment]::SetEnvironmentVariable('XDG_CACHE_HOME', "$HOME\.cache", [System.EnvironmentVariableTarget]::User)
+}
+
 if ($Registry) {
   Install-Registry
+  return
+}
+
+if ($Environment) {
+  Install-Environment
   return
 }
 
--- windows/profile.ps1
+++ windows/profile.ps1
@@ -1,2 +1,4 @@
 ﻿Import-Module posh-git
 $GitPromptSettings.BranchIdenticalStatusSymbol = ''
+
+oh-my-posh init pwsh --config "$env:XDG_CONFIG_HOME\oh-my-posh\config.json" | Invoke-Expression
